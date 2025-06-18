d64233d05841782db517b2fda3c5af79234f6726
FikriBaihaqi73 <fikrimuhammad38993@gmail.com>
2025-06-18 09:16:41 +0700

    refactor: remove HTML structure from 404 error view for cleaner presentation

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,24 +6,3 @@
 @section('suggestion', $suggestion ?? '')
 @section('dynamic_info', $dynamicInfo ?? '')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/js/app.js'])
-</head>
-<body>
-    <div class="text-zone">
-        <h1>{{ $status_code ?? '404' }}</h1>
-        <p>{{ $errorMessage ?? 'Halaman yang Anda cari tidak ditemukan.' }}</p>
-        @isset($suggestion)
-            <p>{{ $suggestion }}</p>
-        @endisset
-        @isset($dynamicInfo)
-            <p>{{ $dynamicInfo }}</p>
-        @endisset
-        <a href="/">Kembali ke Beranda</a>
-    </div>
-</body>
-</html>
