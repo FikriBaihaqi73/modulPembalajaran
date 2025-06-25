94ceb160eb2e160d1a81d8571e4afaa75df33bf1
FikriBaihaqi73 <fikrimuhammad38993@gmail.com>
2025-06-25 16:33:38 +0700

    feat: add technology images to ModuleSeeder for dynamic thumbnail assignment

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -20,6 +20,15 @@ class ModuleSeeder extends Seeder
         $faker = Faker::create();
         $majorIds = Major::pluck('id')->all();
 
+        $technologyPexelsImages = [
+            'https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            'https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            'https://images.pexels.com/photos/2007604/pexels-photo-2007604.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            'https://images.pexels.com/photos/416405/pexels-photo-416405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            'https://images.pexels.com/photos/2653362/pexels-photo-2653362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        ];
+
         $mentor = User::whereHas('role', function($query) {
             $query->where('name', 'Mentor');
         })->first();
@@ -44,7 +53,7 @@ class ModuleSeeder extends Seeder
                     $module = Module::create([
                         'name' => $faker->unique()->sentence(rand(3, 6)),
                         'content' => $faker->sentences(rand(3, 5), true),
-                        'thumbnail' => 'https://via.placeholder.com/640x480.png/00dddd?text=module+' . $faker->randomNumber(2),
+                        'thumbnail' => $faker->randomElement($technologyPexelsImages),
                         'major_id' => $category->major_id,
                         'user_id' => $mentor->id,
                         'is_visible' => $faker->boolean(90),
